015f680be31778fc5236f5cf45145cd85a882208
AYON <nafismahmudayon@gmail.com>
2024-10-26 22:14:24 +0600

    testing theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,6 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
-  presets: [require('./app/src/theme/default-preset.ts')],
-  plugins: []
+  ]
 }
 export default AspectUITheme(config)
