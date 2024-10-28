14312ae23ef5c8e2307968d4bed3a6d47d3a32f6
AYON <nafismahmudayon@gmail.com>
2024-10-28 20:02:34 +0600

    Modal complete

--- app/src/components/Modal/ModalActions.tsx
+++ app/src/components/Modal/ModalActions.tsx
@@ -2,17 +2,19 @@
 
 import React, { ReactNode } from 'react'
 import { useModal } from './ModalContext'
+import { cn } from '../../utils/cn'
 
 interface ModalActionProps {
   children: ReactNode
+  className?: string
 }
 
-export const ModalAction: React.FC<ModalActionProps> = ({ children }) => {
+export const ModalAction: React.FC<ModalActionProps> = ({ children, className='' }) => {
   const { handleOpen } = useModal()
 
   return (
     <button
-      className='rounded bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-600'
+      className={cn('rounded  px-4 py-2 transition-colors bg-primary-100 dark:bg-primary-900 hover:bg-primary-200 dark:hover:bg-primary-800 text-primary-800 dark:text-primary-200 hover:text-primary-900 dark:hover:text-primary-100', className)}
       onClick={handleOpen}
     >
       {children}
--- components/test/Modal.tsx
+++ components/test/Modal.tsx
@@ -7,9 +7,11 @@ const ModalDemo = () => {
       <Modal>
         <ModalAction>Open Modal</ModalAction>
         <ModalContent>
-          <h2 className='mb-2 text-2xl font-bold'>Modal Title</h2>
-          <p className='mb-4'>This is the modal content.</p>
-          <ModalAction>Close</ModalAction>
+          <div className='border p-4 rounded-md bg-primary-100 text-primary-800'>
+            <h2 className='mb-2 text-2xl font-bold'>Modal Title</h2>
+            <p className='mb-4'>This is the modal content.</p>
+            <ModalAction>Close</ModalAction>
+          </div>
         </ModalContent>
       </Modal>
       <Modal>
