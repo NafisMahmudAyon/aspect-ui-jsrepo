cb0fbf54f1921507615be2a6fbcebf7dc388865b
AYON <nafismahmudayon@gmail.com>
2024-11-05 20:07:09 +0600

    Textarea Complete

--- lib/esm/components/Textarea/Textarea.d.ts
+++ lib/esm/components/Textarea/Textarea.d.ts
@@ -3,6 +3,9 @@ interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement
     label?: string;
     error?: string;
     className?: string;
+    labelClassName?: string;
+    wrapperClassName?: string;
+    errorClassName?: string;
 }
 export declare const Textarea: React.ForwardRefExoticComponent<TextareaProps & React.RefAttributes<HTMLTextAreaElement>>;
 export {};
